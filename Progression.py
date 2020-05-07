a276757d27ce6b0e98d21d19012c177b89c839b2
c3nsored <ur.gabriel@outlook.com>
2020-05-07 15:19:42 +0200

    worked on inheritence

---
+++ Progression.py
@@ -0,0 +1,40 @@
+class Progression:
+    """ Iterator producing a generic progression """
+
+    def __init__(self, start=0):
+
+        self._current = start   # Initialize the current value to start
+
+    def _advance(self):
+        """ Update self._current to a new value """
+        self._current += 1
+
+    def __next__(self):
+        """Return the next element, or else raise StopIteration Error """
+        if self._current is None:   # our convention to end a proogression
+            raise StopIteration()
+        else:
+            answer = self._current
+            self._advance()
+            return answer
+
+    def __iter__(self):
+        """By convention an iter must return itself as an iterator """
+        return self
+    
+    def print_progression(self):
+        """ Print next n value of the progression """
+        print(''.join(str(next(self)) for _ in range(n)))
+
+
+class ArithmeticProgression(Progression):
+    """Creates an arithemtic progression """
+
+    def __init__(self, increment=1, start=0):
+        """ Create a new arithmetic progression:
+            increment: the constant to add to each term 
+            start: the starting point of each progresson """ 
+        super().__init__(start)
+        self._increment = increment
+    
+    def 
--- ZitCard.py
+++ ZitCard.py
@@ -3,10 +3,7 @@
 class ZitCard:
     """ An internet card """
     def __init__(self, bank, acnt, limit, customer):
-        """ Create a new Zit card instance 
-
-        The initial balance is 0
-        """
+        """ Create a new Zit card instance. The initial balance is 0"""
         self._bank = bank
         self._acnt = acnt
         self._limit = limit
@@ -30,7 +27,6 @@ class ZitCard:
     
     def charge(self, price):
         """ Charge given price to the card, assuming sufficient balance """
-
         if price + self._balance > self._limit: # if charge would exceed limit
             return False                        # Cannot accept payment
         else:
@@ -39,7 +35,40 @@ class ZitCard:
 
     def make_payment(self, amount):
         self._balance -= amount
+
+
+class PredatoryCreditCard(ZitCard):
+    def __init__(self, bank, acnt, limit, customer, apr):
+        """ Create a new predatory credit card instance 
+            
+            customer: name of customer 
+            bank: name of the bank 
+            limit: credit limit (int)
+            acnt: account number (String)
+            apr: Annual precentage rate (float)
+        """
+        super().__init__(bank, acnt, limit, customer) # call super class constructor
+        self._apr = apr 
+
+    def charge(self, price):
+        """Charge a price to the card assuming sufficient credit limit """
+        # Return true if charge is processed
+        # Return false and assess a 5$ fee if charge is denied 
+        success = super().charge(price)
+        if not success:
+            # Charge 5$
+            self._balance += 5 
+        return success
     
+    def process_month(self):
+        """Assess monthly interset on outstanding balance """
+        if self._balance > 0:
+            # if positive balance, convert APD to monthly multiplicative factor 
+            monthly_factor = pow(1 + self._apr, 1/12)
+            self._balance *= monthly_factor
+    
+
+
 if __name__ == "__main__":
     """ Run tests """
     cc = ZitCard('1st Bank', '5391 0375 9387 5309', 1000, "John Doe")
@@ -49,9 +78,9 @@ if __name__ == "__main__":
     wallet.append(ZitCard('1st Bank,', '5391 0375 9387 7309', 1000, "Richard lam"))
     
     print(len(wallet))
-    if cc:
-        print('True')
-    print(len(cc)) # Will return type error 
+    # if cc:
+    #     print('True')
+    # print(len(cc)) # Will return type error 
     
 
     for val in range(1, 17):
@@ -69,4 +98,3 @@ if __name__ == "__main__":
             wallet[c].make_payment(100)
             print('New balance =', wallet[c].get_balance())
         print()
-        
