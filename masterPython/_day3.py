0a36ccb35d5f5231010304d62f88e99c7778e3c0
c3nsored <ur.gabriel@outlook.com>
2020-11-20 08:01:11 +0100

    day 3 edits

--- masterPython/_day3.py
+++ masterPython/_day3.py
@@ -2,7 +2,7 @@ x = [1, 3, 2, 5, 4, 9, 6]
 _y = range(len(x))
 y = list(range(len(x)))
 
-def sort_x(l) -> list:
+def sort_x(l):
     n = len(l)
     for i in range(n - 1):
         if l[i] > l[i+ 1]:
@@ -10,6 +10,11 @@ def sort_x(l) -> list:
     else:
         # print('it broke')
         return
-print(type(_y))
-print(type(y))
-print(type(sort_x))
+
+print(type(_y))  # class range
+print(type(y))  # class list
+print(type(sort_x))  # class function
+
+class NewFunc(function):
+    def __init__(self):
+        
